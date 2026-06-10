c7d440de98128170bbd3331e4ce9806b70744884
Dipak Poudel <deepak.poudel077@gmail.com>
2026-06-11 00:48:13 +0545

    commands for tools

---
+++ tools-installation.sh
@@ -0,0 +1,84 @@
+# install Docker 
+sudo apt update
+
+sudo apt install -y docker.io
+
+sudo usermod -aG docker $USER
+
+
+# install Kubectl
+
+sudo apt-get update
+
+sudo apt-get install -y apt-transport-https ca-certificates curl gnupg -y
+
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.36/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+
+sudo chmod 644 /etc/apt/keyrings/kubernetes-apt-keyring.gpg # allow unprivileged APT programs to read this keyring
+
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.36/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+sudo chmod 644 /etc/apt/sources.list.d/kubernetes.list   # helps tools such as command-not-found to work correctly
+
+sudo apt-get update
+
+sudo apt-get install -y kubectl 
+
+
+# Install JAVA
+
+sudo apt update
+
+sudo apt install -y default-jre
+
+java -version
+
+
+# Install AWSCLI
+sudo apt -y update
+
+sudo apt install unzip
+
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+
+unzip awscliv2.zip
+
+sudo ./aws/install
+
+
+# Install terraform
+
+# Update package list and install dependencies
+sudo apt update && sudo apt install -y gnupg software-properties-common curl
+
+# Download and add the official HashiCorp GPG key
+curl -fsSL https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/null
+
+# Add the official HashiCorp repository to your system sources
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+
+# Update package lists again and install Terraform
+sudo apt update && sudo apt install -y terraform
+
+
+# Install trivy
+sudo apt update
+
+curl -sfL https://raw.githubusercontent.com/aquasecurity/trivy/main/contrib/install.sh | sudo sh -s -- -b /usr/local/bin v0.71.1
+
+sudo apt-get install wget gnupg
+wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | gpg --dearmor | sudo tee /usr/share/keyrings/trivy.gpg > /dev/null
+echo "deb [signed-by=/usr/share/keyrings/trivy.gpg] https://aquasecurity.github.io/trivy-repo/deb generic main" | sudo tee -a /etc/apt/sources.list.d/trivy.list
+sudo apt-get update
+sudo apt-get install trivy
+
+
+# run sonarqube using docker
+sudo apt update
+
+newgrp docker
+
+docker run -d --name sonarqube -p 9000:9000 sonarqube
+
+docker ps
+
